2cc68cb21631e82036f86c5d6636fcc6b49a2da7
Abhi-Trivedi05 <abhitrivedi2005@gmail.com>
2025-04-19 16:14:36 +0530

    Changes to be committed: 	modified:   my-app/next.config.mjs 	modified:   my-app/package-lock.json 	modified:   my-app/package.json

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -8,6 +8,33 @@ const nextConfig = {
   },
   images: {
     unoptimized: true,
+    domains: ['localhost'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  poweredByHeader: false,
+  reactStrictMode: true,
+  compress: true,
+  output: 'standalone',
+  experimental: {
+    optimizePackageImports: ['@radix-ui/react-icons'],
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          { key: 'X-DNS-Prefetch-Control', value: 'on' },
+          { key: 'X-XSS-Protection', value: '1; mode=block' },
+          { key: 'X-Frame-Options', value: 'SAMEORIGIN' },
+          { key: 'X-Content-Type-Options', value: 'nosniff' },
+        ],
+      },
+    ];
   },
 }
 
